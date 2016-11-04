0eaea1da49db8ea63d26885fa8c82d0ec1e8aca9
Julien Evano <julienevano@yahoo.com.au>
2016-11-04 20:49:50 +1100

    Step 2 - Extend Ionic App Scripts copy and webpack task configs

---
+++ config/webpack.config.js
@@ -0,0 +1,6 @@
+var ionicWebpackConfig = require('@ionic/app-scripts/config/webpack.config');
+var webpackMerge = require('webpack-merge');
+
+// Webpack config
+module.exports = webpackMerge(ionicWebpackConfig, {
+});
