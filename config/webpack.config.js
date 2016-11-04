cc8801c1eae8dbbbac6b9b9a7fe11834045ab84e
Julien Evano <julienevano@yahoo.com.au>
2016-11-04 21:26:31 +1100

    Step 4 - Extend Webpack config - HtmlWebpackPlugin config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,28 @@
 var ionicWebpackConfig = require('@ionic/app-scripts/config/webpack.config');
+var manifest = require('../src/manifest.json');
 var webpackMerge = require('webpack-merge');
+
+//  Webpack plugins
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Webpack config
 module.exports = webpackMerge(ionicWebpackConfig, {
+  plugins: [
+    /*
+    * Plugin: HtmlWebpackPlugin
+    * Description: Simplifies creation of HTML files to serve your webpack bundles.
+    * This is especially useful for webpack bundles that include a hash in the filename
+    * which changes every compilation.
+    *
+    * See: https://github.com/ampedandwired/html-webpack-plugin
+    */
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      chunksSortMode: 'dependency',
+      options: {
+        title: manifest.name
+      }
+    })
+  ]
 });
