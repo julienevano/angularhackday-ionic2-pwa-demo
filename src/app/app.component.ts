d92870c7887b242fa4a02ff6a47b7c521ceca2f5
Julien Evano <julienevano@yahoo.com.au>
2016-11-04 21:38:46 +1100

    Step 4 - Extend Webpack config - Add a platform condition before setup cordova plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,12 @@ export class MyApp {
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
+      if (platform.is('cordova')) {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        StatusBar.styleDefault();
+        Splashscreen.hide();
+      }
     });
   }
 }
