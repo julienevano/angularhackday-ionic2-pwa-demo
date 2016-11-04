2dc603b25f68a1ffadc04da4df7a8ead46761f72
Julien Evano <julienevano@yahoo.com.au>
2016-11-04 21:29:10 +1100

    Step 4 - Extend Webpack config - FaviconsWebpackPlugin config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,6 +3,7 @@ var manifest = require('../src/manifest.json');
 var webpackMerge = require('webpack-merge');
 
 //  Webpack plugins
+var FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Webpack config
@@ -22,6 +23,31 @@ module.exports = webpackMerge(ionicWebpackConfig, {
       chunksSortMode: 'dependency',
       options: {
         title: manifest.name
+      }
+    }),
+
+    /*
+     * Plugin: FaviconsWebpackPlugin
+     * Description: Allows to use the favicons generator with webpack.
+     *
+     * See: https://webpack.github.io/docs/list-of-plugins.html#faviconswebpackplugin
+     */
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, '../resources/icon.png'),
+      prefix: 'assets/icons/favicons-[hash]/',
+      background: manifest.background_color,
+      title: manifest.name,
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        opengraph: false,
+        twitter: true,
+        yandex: false,
+        windows: false
       }
     })
   ]
